cea17036edeff6dd93eda9189a95a1f1ce8e28e6
TheHarrySun <hsx925@gmail.com>
2025-12-13 04:12:31 -0500

    fixing request to be an officer buy

--- frontend/src/components/ClubCard.jsx
+++ frontend/src/components/ClubCard.jsx
@@ -72,6 +72,7 @@ export default function ClubCard({
                         onDelete?.(club, e);
                     }}
                     onRequestOfficer={() => {
+                        setShowModal(false);
                         onRequestOfficer?.(club);
                     }}
                     onPostClick={(post) => {
